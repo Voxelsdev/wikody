1e3701c8a88e3af505c3791c72e54e67639632ee
Tyler Miller <tylermillerhome@gmail.com>
2016-09-08 10:28:48 -0700

    Fixed buttons, Stylized more hmtl

--- js/home.js
+++ js/home.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
-
   function storeData(event) {
     localStorage.numPlayerDivs = parseInt($(event.target).text().substring(0,1));
   }
-  
   $('.players').on('click', storeData);
 });
